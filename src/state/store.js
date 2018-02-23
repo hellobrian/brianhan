b61d143f67410a6e8ccdcd7b667f3d0dbdd2e422
Brian Han <brianhan87usa@gmail.com>
2018-02-23 01:14:09 -0600

    perf: pwa stuff

--- src/state/store.js
+++ src/state/store.js
@@ -4,6 +4,9 @@ import { reducer } from 'state/nav/reducers';
 const reduxDevTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = createStore(reducer, reduxDevTools);
+const store =
+  process.env.NODE_ENV === 'development'
+    ? createStore(reducer, reduxDevTools)
+    : createStore(reducer);
 
 export default store;
