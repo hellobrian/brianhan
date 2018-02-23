21edccf320422d4d928a6001bc8ca5d308d2b5ba
Brian Han <brianhan87usa@gmail.com>
2018-02-22 22:57:31 -0600

    feat: setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from "react";
+import { Route, Switch, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import UsesPage from "./pages/UsesPage";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <HomePage />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/uses" component={UsesPage} />
+      </Switch>
     );
   }
 }
--- src/pages/UsesPage.js
+++ src/pages/UsesPage.js
@@ -1,12 +1,11 @@
 import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
-
-export default class HomePage extends Component {
+export default class UsesPage extends Component {
   render() {
     return (
       <Fragment>
-        Home Page
-        <Link to="/uses">UsesPage</Link>
+        Uses Page
+        <Link to="/">Home Page</Link>
       </Fragment>
     );
   }
