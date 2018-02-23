41e89c2b939a2618856fddb24d2197b3a040c6e3
Brian Han <brianhan87usa@gmail.com>
2018-02-22 23:24:07 -0600

    feat: setup react-loadable
    codesplitting per route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
-import { Route, Switch, Link } from "react-router-dom";
-import logo from "./logo.svg";
+import { Route, Switch } from "react-router-dom";
+import Loadable from "react-loadable";
 import "./App.css";
-import HomePage from "./pages/HomePage";
-import UsesPage from "./pages/UsesPage";
+
+const Loading = ({ error, pastDelay }) => {
+  if (error) {
+    return <div>Error!</div>;
+  } else if (pastDelay) {
+    return <div>Loading...</div>;
+  } else {
+    return null;
+  }
+};
+
+const Home = Loadable({
+  loader: () => import("./pages/HomePage"),
+  loading: Loading
+});
+
+const Uses = Loadable({
+  loader: () => import("./pages/UsesPage"),
+  loading: Loading
+});
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/uses" component={UsesPage} />
+        <Route exact path="/" component={Home} />
+        <Route path="/uses" component={Uses} />
       </Switch>
     );
   }
